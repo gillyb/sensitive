3af403f3e4e93ab098a8d96f97cc0063cda3ce9f
Gilly Barr <gilly@logz.io>
2016-03-15 20:18:34 +0200

    fixing copy/paste

--- release/Sensitive-darwin-x64/Sensitive.app/Contents/Resources/app/main.js
+++ release/Sensitive-darwin-x64/Sensitive.app/Contents/Resources/app/main.js
@@ -6,6 +6,7 @@ const electron = require('electron');
 const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
+const Menu = require('menu');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -38,6 +39,21 @@ function createWindow () {
     });
 
   }, 0);
+
+  var template = [{
+    label: "Edit",
+    submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 // This method will be called when Electron has finished
